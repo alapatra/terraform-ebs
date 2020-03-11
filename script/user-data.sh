e29416ced6761a170bf2197efdcfc741d016da7c
alapatra <sbs.timalsina@gmail.com>
2020-03-10 20:44:36 -0400

    Update user-data.sh

--- script/user-data.sh
+++ script/user-data.sh
@@ -1,16 +1,17 @@
-vol="vol-03b76bce20fb6e760"
-mount_dir =" /subash"
-device="/dev/xvdf/"
-
+#!/bin/bash
+pip install awscli
+vol="vol-0d36907ff06fed912"
+mount_dir="/subash"
+device="/dev/xvdf"
+region="us-east-1"
 if [ ! -d $mount_dir ]
 then 
-mkdir $mount_dir
-fi
+mkdir $mount_dir 
+fi 
 echo "this command is to fetch instance id"
-intance_id = `curl http://169.254.169.254/latest/meta-data/instance-id`
-
+instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
 echo "this command is to attach EBS to EC2"
-aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf
-
-sleep 60
+aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf --region $region
+sleep 60 
+echo "mount filesystem to folder" 
 mount $device $mount_dir
